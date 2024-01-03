a014d7c35c65de03c021d6e880db0d01357903b2
FRickReich <frickreich@gmail.com>
2024-01-03 11:45:24 +0100

    Add characters to window display

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,21 @@ const App = () => {
 
 	}, [currentIngredient]);
 
+	const getRandomImage = () => 
+	{
+		const images = [
+			"https://static.wikia.nocookie.net/disney/images/2/27/Goofy_transparent.png",
+			"https://static.wikia.nocookie.net/disney/images/d/db/Donald_Duck_Iconic.png",
+			"https://static.wikia.nocookie.net/disney/images/b/bf/Mickey_Mouse_Disney_1.png",
+			"https://static.wikia.nocookie.net/disney/images/3/36/Minnie_Mouse_pose_.jpg",
+			"https://static.wikia.nocookie.net/disney/images/0/0a/Scrooge_McDuck.jpeg",
+			"https://static.wikia.nocookie.net/disney/images/6/67/Daisy_Duck_transparent.png",
+			"https://static.wikia.nocookie.net/disney/images/4/43/Huey_Dewey_Louie_artwork.jpg"
+		];
+
+		return images[(Math.random() * images.length) | 0]
+	}
+
 	const filteredRecipes = recipesData.recipes
 		.filter(recipe => {
 			const matchTitle = recipe.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -117,6 +132,9 @@ const App = () => {
 								<p>Verkauf: {selectedIngredient.revenue} <FontAwesomeIcon icon={faCoins}/></p>
 							</div>
 							<button className="App__window__button" onClick={() => handleHideIngredientsWindow()}>Danke</button>
+							<div className="App__window__character">
+								<img src={getRandomImage()} alt="" />
+							</div>
 						</div>
 					</div>
 				)
